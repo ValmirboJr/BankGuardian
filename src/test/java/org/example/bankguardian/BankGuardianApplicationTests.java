9a22b6538ef3ac0a3b8dd8727231966edffe84e5
ValmirboJr <valmirborgesojr@gmail.com>
2025-12-11 23:50:03 -0300

    feat:iniciando projeto

---
+++ src/test/java/org/example/bankguardian/BankGuardianApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.bankguardian;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankGuardianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
