e91621756f89570221475c920351cdb9117848e6
ValmirboJr <valmirborgesojr@gmail.com>
2025-12-11 23:49:42 -0300

    feat:iniciando projeto

---
+++ src/main/java/org/example/bankguardian/BankGuardianApplication.java
@@ -0,0 +1,14 @@
+package org.example.bankguardian;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankGuardianApplication.class, args);
+    }
+
+}
