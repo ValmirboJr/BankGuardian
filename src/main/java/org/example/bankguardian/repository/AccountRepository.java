63e71be4b845a21fdf06e32b2661ff62d760a0a6
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:04:03 -0300

    feat:Criando Repository para comunicar com a entidade

--- src/main/java/org/example/bankguardian/repository/AccountRepository.java
+++ src/main/java/org/example/bankguardian/repository/AccountRepository.java
@@ -1,4 +1,14 @@
 package org.example.bankguardian.repository;
 
-public interface AccountRepository {
+import org.example.bankguardian.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+    Optional<Account> findByEmail(String email);
+    Optional<Account> getAccountsById(UUID id);
 }
