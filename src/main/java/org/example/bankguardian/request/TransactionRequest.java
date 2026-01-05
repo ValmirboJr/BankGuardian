1381636ccc7fa9ebfd439ad7ea07fc3fce4ab5af
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:14:27 -0300

    feat:Criando request para validação dos dados para criação das entidades e mapeamento das mesmas

--- src/main/java/org/example/bankguardian/request/TransactionRequest.java
+++ src/main/java/org/example/bankguardian/request/TransactionRequest.java
@@ -1,4 +1,15 @@
 package org.example.bankguardian.request;
 
-public class TransactionRequest {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.example.bankguardian.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public record TransactionRequest(UUID accountId, @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime eventTime, String description,
+                                 BigDecimal amount,@JsonInclude PaymentMethod paymentMethod) {
 }
