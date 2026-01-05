ff4e58458e0e0b32035ce43376d01aef9f59436f
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 08:56:20 -0300

    feat:Criando entidade Account

--- src/main/java/org/example/bankguardian/entity/Account.java
+++ src/main/java/org/example/bankguardian/entity/Account.java
@@ -1,4 +1,36 @@
 package org.example.bankguardian.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountid;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Transaction> transactions = new ArrayList<>();
 }
