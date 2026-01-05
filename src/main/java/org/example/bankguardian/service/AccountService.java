51da8e18bb4998fa2da9b824d991f7935c3b94fb
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:11:23 -0300

    feat:Criando service para criação das regras de negócio

--- src/main/java/org/example/bankguardian/service/AccountService.java
+++ src/main/java/org/example/bankguardian/service/AccountService.java
@@ -1,4 +1,44 @@
 package org.example.bankguardian.service;
 
+import org.example.bankguardian.entity.Account;
+import org.example.bankguardian.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account) {
+        if(accountRepository.findByEmail(account.getEmail()).isPresent()){
+            return accountRepository.findByEmail(account.getEmail()).get();
+        }
+    return accountRepository.save(account);
+    }
+
+    public Optional<Account> findById(UUID id) {
+        return accountRepository.findById(id);
+    }
+    public List<Account> ListAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Optional<Account> updateAccount(UUID id, Account AccountUpdate) {
+        Optional<Account> Update = accountRepository.getAccountsById(id);
+        if (Update.isPresent()) {
+            Account Accounts = Update.get();
+            accountRepository.save(AccountUpdate);
+            return Optional.of(AccountUpdate);
+        }
+        return Optional.empty();
+    }
+    public void deleteAccount(UUID id) {
+        accountRepository.deleteById(id);
+    }
 }
