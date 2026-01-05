d2de5d3673c5a4a21cefed5d74e1ff74acd157a4
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:16:28 -0300

    feat:Criando response para controle dos dados a serem retornados

--- src/main/java/org/example/bankguardian/response/TransactionResponse.java
+++ src/main/java/org/example/bankguardian/response/TransactionResponse.java
@@ -1,4 +1,15 @@
 package org.example.bankguardian.response;
 
-public record TransactionResponse() {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.example.bankguardian.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+public record TransactionResponse(UUID transactionId, UUID accountid,@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime eventTime, String description,
+                                  BigDecimal amount, @JsonInclude PaymentMethod paymentMethod) {
 }
