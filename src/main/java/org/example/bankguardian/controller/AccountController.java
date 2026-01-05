b7ddb14da9772ad429b81e7a45a63b219137105c
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:19:20 -0300

    feat:criando controller para acessar os endpoints para as regras de account

--- src/main/java/org/example/bankguardian/controller/AccountController.java
+++ src/main/java/org/example/bankguardian/controller/AccountController.java
@@ -1,4 +1,48 @@
 package org.example.bankguardian.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.example.bankguardian.entity.Account;
+import org.example.bankguardian.mapper.AccountMapper;
+import org.example.bankguardian.request.AccountRequest;
+import org.example.bankguardian.response.AccountResponse;
+import org.example.bankguardian.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/criar")
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountRequest request) {
+        Account account = accountService.createAccount(AccountMapper.toAccount(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(AccountMapper.toAccountResponse(account));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponse>> Allaccounts() {
+        return ResponseEntity.ok(accountService.ListAccounts()
+                .stream().map(AccountMapper::toAccountResponse).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponse> GetAccount(@PathVariable UUID id) {
+        return ResponseEntity.of(accountService.findById(id)
+                .map(AccountMapper::toAccountResponse));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable UUID id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
