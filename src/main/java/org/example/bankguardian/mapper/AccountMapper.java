7ecfe1f80855fc3d16ea86ab3eb843d378feecf6
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:18:22 -0300

    feat:criando mapper para isolar o domínio da camada de transporte e centralizar a transformação dos dados

--- src/main/java/org/example/bankguardian/mapper/AccountMapper.java
+++ src/main/java/org/example/bankguardian/mapper/AccountMapper.java
@@ -1,4 +1,39 @@
 package org.example.bankguardian.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.bankguardian.entity.Account;
+import org.example.bankguardian.request.AccountRequest;
+import org.example.bankguardian.response.AccountResponse;
+import org.example.bankguardian.response.TransactionResponse;
+
+import java.util.List;
+
+@UtilityClass
 public class AccountMapper {
+
+    public static Account toAccount(AccountRequest request) {
+
+        return Account
+                .builder()
+                .email(request.email())
+                .password(request.password())
+                .username(request.username())
+                .build();
+    }
+
+    public static AccountResponse toAccountResponse(Account response) {
+
+        List<TransactionResponse> transactions = response.getTransactions()
+                .stream()
+                .map(TransactionMapper::toTransactionResponse)
+                .toList();
+
+        return AccountResponse
+                .builder()
+                .accountid(response.getAccountid())
+                .username(response.getUsername())
+                .email(response.getEmail())
+                .transactions(transactions)
+                .build();
+    }
 }
--- src/main/java/org/example/bankguardian/mapper/TransactionMapper.java
+++ src/main/java/org/example/bankguardian/mapper/TransactionMapper.java
@@ -1,4 +1,38 @@
 package org.example.bankguardian.mapper;
 
+import lombok.experimental.UtilityClass;
+import org.example.bankguardian.entity.Account;
+import org.example.bankguardian.entity.Transaction;
+import org.example.bankguardian.request.TransactionRequest;
+import org.example.bankguardian.response.TransactionResponse;
+
+@UtilityClass
 public class TransactionMapper {
+
+    public static Transaction toTransaction(TransactionRequest transactionRequest) {
+
+        Account account = new Account();
+        account.setAccountid(account.getAccountid());
+
+        return Transaction
+                .builder()
+                .eventTime(transactionRequest.eventTime())
+                .description(transactionRequest.description())
+                .amount(transactionRequest.amount())
+                .paymentMethod(transactionRequest.paymentMethod())
+                .build();
+    }
+
+    public static TransactionResponse toTransactionResponse(Transaction transaction) {
+
+        return TransactionResponse
+                .builder()
+                .transactionId(transaction.getTransactionId())
+                .accountid(transaction.getAccount().getAccountid())
+                .eventTime(transaction.getEventTime())
+                .description(transaction.getDescription())
+                .amount(transaction.getAmount())
+                .paymentMethod(transaction.getPaymentMethod())
+                .build();
+    }
 }
