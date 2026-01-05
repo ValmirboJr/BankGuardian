f7e97c6f5ef878f681ff7556a2e2eea900f2a415
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:12:02 -0300

    feat:Criando service para criação das regras de negócio

--- src/main/java/org/example/bankguardian/service/TransactionService.java
+++ src/main/java/org/example/bankguardian/service/TransactionService.java
@@ -1,4 +1,48 @@
 package org.example.bankguardian.service;
 
+import org.example.bankguardian.entity.Account;
+import org.example.bankguardian.entity.Transaction;
+import org.example.bankguardian.mapper.TransactionMapper;
+import org.example.bankguardian.repository.AccountRepository;
+import org.example.bankguardian.repository.TransactionRepository;
+import org.example.bankguardian.request.TransactionRequest;
+import org.example.bankguardian.response.TransactionResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public TransactionResponse criarTransacao(TransactionRequest request){
+
+        Account account = accountRepository.findById(request.accountId())
+                .orElseThrow(() -> new RuntimeException("Dados não encontrados"));
+        Transaction transaction = TransactionMapper.toTransaction(request);
+        transaction.setAccount(account);
+        transaction = transactionRepository.save(transaction);
+        TransactionResponse response = TransactionMapper.toTransactionResponse(transaction);
+       // transactionProducer.sendTransactionCreated(response);
+        return response;
+    }
+
+    public Optional<Transaction> getTrn(UUID id) {
+        return transactionRepository.findById(id);
+    }
+    public List<Transaction> getAllTrn() {
+        return transactionRepository.findAll();
+    }
+    public void DeleteTrn(UUID id) {
+        transactionRepository.deleteById(id);
+    }
+
 }
