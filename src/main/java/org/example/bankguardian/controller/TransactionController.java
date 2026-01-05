26ba9657127f7c61d0f11986a1bcc457f2130407
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-05 09:19:42 -0300

    feat:criando controller para acessar os endpoints para as regras de transaction

--- src/main/java/org/example/bankguardian/controller/TransactionController.java
+++ src/main/java/org/example/bankguardian/controller/TransactionController.java
@@ -1,4 +1,43 @@
 package org.example.bankguardian.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.bankguardian.mapper.TransactionMapper;
+import org.example.bankguardian.request.TransactionRequest;
+import org.example.bankguardian.response.TransactionResponse;
+import org.example.bankguardian.service.TransactionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/trn")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/criar")
+    public ResponseEntity<TransactionResponse> register(@RequestBody TransactionRequest request){
+        return ResponseEntity.ok(transactionService.criarTransacao(request));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TransactionResponse> getTransactions(@RequestParam UUID id){
+        return ResponseEntity.of(transactionService.getTrn(id)
+                .map(TransactionMapper::toTransactionResponse));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponse>> getTransactions(){
+        return ResponseEntity.ok(transactionService.getAllTrn()
+                .stream().map(TransactionMapper::toTransactionResponse).toList());
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TransactionResponse> deleteTransactions(@RequestParam UUID id){
+        transactionService.DeleteTrn(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
