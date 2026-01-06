03891d414bb74b7ee8c2fb3d8817c8230b955182
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-06 09:03:13 -0300

    feat:Adicionando authenticação com JWT

---
+++ src/main/java/org/example/bankguardian/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.example.bankguardian.config;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/org/example/bankguardian/exception/UsernameOrPasswordInvalidException.java
@@ -0,0 +1,7 @@
+package org.example.bankguardian.exception;
+
+public class UsernameOrPasswordInvalidException extends RuntimeException {
+  public UsernameOrPasswordInvalidException(String message) {
+    super(message);
+  }
+}
