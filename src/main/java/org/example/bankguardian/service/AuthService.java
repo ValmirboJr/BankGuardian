72b5316d4a54cd44e74e31ae75485c3cb1ae97c3
ValmirboJr <valmirborgesojr@gmail.com>
2026-01-08 08:14:35 -0300

    feat:Adicionando metodo de authenticação

--- src/main/java/org/example/bankguardian/service/AuthService.java
+++ src/main/java/org/example/bankguardian/service/AuthService.java
@@ -1,4 +1,20 @@
 package org.example.bankguardian.service;
 
-public class AuthService {
+import org.example.bankguardian.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return accountRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException("email ou senha invalidos"));
+    }
 }
